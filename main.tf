a7c27e6e3cba3be0ec86fa5dd66ee0141ec2d650
Kaustubh <kaustubhkhavnekar@gmail.com>
2021-04-04 00:53:02 +0530

    Added explicit dependencies

--- main.tf
+++ main.tf
@@ -18,25 +18,31 @@ data "aws_security_group" "default" {
 }
 
 module "vpc" {
-  source                                   = "terraform-aws-modules/vpc/aws"
-  version                                  = "~> 2.0"
-  name                                     = var.vpc_name
-  cidr                                     = var.vpc_cidr
-  azs                                      = var.vpc_azs
-  private_subnets                          = var.vpc_private_subnets
-  public_subnets                           = var.vpc_public_subnets
-  enable_dns_hostnames                     = true
-  enable_dns_support                       = true
-  enable_nat_gateway                       = false
-  enable_ssm_endpoint                      = !var.is_public_instance
-  ssm_endpoint_private_dns_enabled         = var.is_public_instance ? null : true
-  ssm_endpoint_security_group_ids          = [data.aws_security_group.default.id]
+  source                           = "terraform-aws-modules/vpc/aws"
+  version                          = "~> 2.0"
+  name                             = var.vpc_name
+  cidr                             = var.vpc_cidr
+  azs                              = var.vpc_azs
+  private_subnets                  = var.vpc_private_subnets
+  public_subnets                   = var.vpc_public_subnets
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  enable_nat_gateway               = false
+  enable_ssm_endpoint              = !var.is_public_instance
+  ssm_endpoint_private_dns_enabled = var.is_public_instance ? null : true
+  ssm_endpoint_security_group_ids = [
+    data.aws_security_group.default.id
+  ]
   enable_ec2messages_endpoint              = !var.is_public_instance
   ec2messages_endpoint_private_dns_enabled = var.is_public_instance ? null : true
-  ec2messages_endpoint_security_group_ids  = [data.aws_security_group.default.id]
+  ec2messages_endpoint_security_group_ids = [
+    data.aws_security_group.default.id
+  ]
   enable_ssmmessages_endpoint              = !var.is_public_instance
   ssmmessages_endpoint_private_dns_enabled = var.is_public_instance ? null : true
-  ssmmessages_endpoint_security_group_ids  = [data.aws_security_group.default.id]
+  ssmmessages_endpoint_security_group_ids = [
+    data.aws_security_group.default.id
+  ]
 }
 
 module "instance_sg" {
@@ -58,17 +64,21 @@ resource "aws_default_security_group" "default" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    protocol        = "tcp"
-    from_port       = 443
-    to_port         = 443
-    security_groups = [module.instance_sg.this_security_group_id]
+    protocol  = "tcp"
+    from_port = 443
+    to_port   = 443
+    security_groups = [
+      module.instance_sg.this_security_group_id
+    ]
   }
 
   egress {
-    cidr_blocks = [var.vpc_cidr]
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
+    cidr_blocks = [
+      var.vpc_cidr
+    ]
+    protocol  = "tcp"
+    from_port = 443
+    to_port   = 443
   }
 }
 
@@ -77,10 +87,14 @@ module "instance_role" {
   version                 = "~> 3.0"
   create_instance_profile = true
   create_role             = true
-  custom_role_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
-  role_name               = var.iam_role_name
-  role_requires_mfa       = false
-  trusted_role_services   = ["ec2.amazonaws.com"]
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ]
+  role_name         = var.iam_role_name
+  role_requires_mfa = false
+  trusted_role_services = [
+    "ec2.amazonaws.com"
+  ]
 }
 
 
@@ -91,17 +105,20 @@ data "aws_ami" "ubuntu" {
   filter {
     name = "name"
     values = [
-    "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+      "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+    ]
   }
 
   filter {
     name = "virtualization-type"
     values = [
-    "hvm"]
+      "hvm"
+    ]
   }
 
   owners = [
-  "099720109477"]
+    "099720109477"
+  ]
 }
 
 module "ec2_instance" {
@@ -113,8 +130,15 @@ module "ec2_instance" {
   associate_public_ip_address = var.is_public_instance
   user_data                   = var.user_data
   subnet_id                   = var.is_public_instance ? module.vpc.public_subnets[0] : module.vpc.private_subnets[0]
-  vpc_security_group_ids      = [module.instance_sg.this_security_group_id]
-  iam_instance_profile        = module.instance_role.this_iam_instance_profile_name
-  # Required before instance is registered with Systems manager
-  depends_on = [aws_default_security_group.default]
+  vpc_security_group_ids = [
+    module.instance_sg.this_security_group_id
+  ]
+  iam_instance_profile = module.instance_role.this_iam_instance_profile_name
+  # Explicit dependencies on full modules so that they are ready before instance attempts to register with Systems manager
+  depends_on = [
+    aws_default_security_group.default,
+    module.vpc,
+    module.instance_role,
+    module.instance_sg
+  ]
 }
